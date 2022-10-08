847d1d693b0d83038416c43c9af5a86c2d0df9c0
akeeme <akeemeblack@gmail.com>
2022-10-08 00:04:37 -0400

    wokring fine in console

--- API.cs
+++ API.cs
@@ -1,78 +1,83 @@
-using System;
-using System.Threading.Tasks;
-using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text.Json;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System;
+using System.Text.Json.Serialization;
 
+namespace API{
+    class Cars{
+        [JsonPropertyName("Model_Name")]
+        public string? Model_Name { get; set; }
 
-// namespace API{
-//     class Cars{
-//         public string? Model_Name { get; set; }
-//         public string? Make_Name { get; set; }
-//         public int Model_ID { get; set; }
-//     }
+        [JsonPropertyName("Make_Name")]
+        public string? Make_Name { get; set; }
 
-//     public class Program{
-//         private static readonly HttpClient client = new HttpClient();
+        [JsonPropertyName("Model_ID")]
+        public int Model_ID { get; set; }
+    }
+
+    class CarResponse{
+        [JsonPropertyName("Count")]
+        public int Count { get; set; }
+
+        [JsonPropertyName("Message")]
+        public string? Message { get; set; }
+
+        [JsonPropertyName("SearchCriteria")]
+        public string? SearchCriteria { get; set; }
+
+        [JsonPropertyName("Results")]
+        public List<Cars>? Results { get; set; }
+    }
+
+    public class Program{
+        private static readonly HttpClient client = new HttpClient();
         
-//         static async Task Main(string[] args){
-//             await GetCars();
-//         }
+        static async Task Main(string[] args){
+            await GetCars();
+        }
 
-//         static async Task GetCars(){
-//             while (true){
-//                 try{
-//                     Console.WriteLine("Enter Car Manufacturer");
-//                     string? manufacturer = Console.ReadLine();
-
-//                     if(string.IsNullOrWhiteSpace(manufacturer)){
-//                         break;
-//                     }
-
-//                     var result = await client.GetStringAsync($"https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/{manufacturer.ToLower()}?format=json");
-//                     var resultRead = await result.Content.ReadAsStringAsync();
-
-//                     Console.WriteLine(resultRead);
+        static async Task GetCars(){
+            while (true){
+                try{
+                    Console.WriteLine("Enter Car Manufacturer");
+                    string? manufacturer = Console.ReadLine();
 
 
-//                     Console.WriteLine($"The {resultRead?.Make_Name} {resultRead?.Model_Name} has an ID of {resultRead?.Model_ID}");
-//                 }
-//                 catch (HttpRequestException e){
-//                     Console.WriteLine($"Request exception: {e.Message}");
-//                 }
-//             }
-//         }
-//     }
-// }
+                    if(string.IsNullOrWhiteSpace(manufacturer)){
+                        break;
+                    }
 
-// private static readonly HttpClient client = new HttpClient();
-
-// https://vpic.nhtsa.dot.gov/api/vehicles/getallmanufacturers?format=json
-// private static async Task ProcessRepositories(){
+                    Console.WriteLine("Amount of cars to display");
+                    int amount = int.Parse(Console.ReadLine()!);
 
 
-//     while (true)
-//     {
-//         try
-//         {
-//             Console.WriteLine("Enter Car Manufacturer");
+                    // var result = await client.GetAsync($"https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/{manufacturer.ToLower()}?format=json").Result.Content.ReadAsStringAsync();
+                    // var resultRead = JsonSerializer.Deserialize<Cars>(result);
 
-//             var carName = Console.ReadLine();
+                    var result = await client.GetFromJsonAsync<CarResponse>($"https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/{manufacturer.ToLower()}?format=json", new JsonSerializerOptions(JsonSerializerDefaults.Web));
 
-//             if (string.IsNullOrWhiteSpace(carName))
-//             {
-//                 break;
-//             }
+                    if(result?.Count == 0){
+                        Console.WriteLine("No cars found!");
+                        continue;
+                    }
 
-//             var result = await client.GetStringAsync($"https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/{carName.ToLower()}?format=json");
-//             var resultRead = JsonConvert.DeserializeObject<Root>(result);
-//         }
+                    if(result?.Count < amount){
+                        amount = result.Count;
+                    }
 
-//         catch (Exception)
-//         {
-//             Console.WriteLine("ERROR. Please enter a valid car manufacturer.");
-//         }
-//     }
-
-// }
-
+                    Console.WriteLine("Make\t\t|\t\tModel\t\t|\t\tID");
+                    
+                    for (int i = 0; i < amount; i++){
+                        Console.WriteLine($"{result?.Results?[i]?.Make_Name}\t\t\t\t{result?.Results?[i]?.Model_Name}\t\t\t\t{result?.Results?[i]?.Model_ID}");
+                    }
+                }
+                catch (HttpRequestException e){
+                    Console.WriteLine($"Request exception: {e.Message}");
+                }
+            }
+        }
+    }
+}
